1aba6ce57397bde8f87f2a4fbd62200ff0682f9c
sf <sffaster@126.com>
2016-09-10 16:37:06 +0800

    添加首页图片列表显示界面。

---
+++ app/src/main/java/com/sf/funnypuzzle/activity/PuzzleMain.java
@@ -0,0 +1,15 @@
+package com.sf.funnypuzzle.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.sf.funnypuzzle.R;
+
+public class PuzzleMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_puzzle_main);
+    }
+}
